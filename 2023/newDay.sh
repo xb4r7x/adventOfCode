cb9b0524daf1dfea70d14c15b28e3b3958402898
xb4r7x <scully.bart@gmail.com>
2023-11-30 15:18:40 -0700

    Making the setup script slightly less stupid

--- 2023/newDay.sh
+++ 2023/newDay.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-day=$1
+if [ $# -eq 0 ]; then
+    day=$(date +%d)
+    echo "No arguments provided. Assuming today: $day."
+    echo "Ensuring folder for $day doesn't already exist."
+    if [ -d ./$day ]; then
+        echo "The directory for $day already exists. Exiting to prevent overwritting things."
+        exit 1
+    fi
+else
+    day=$1
+fi
+
 year=2023
-mkdir "./$1"
-touch "./$1/ex_input.txt"
-scriptPath="./$1/day$1.py"
+mkdir "./$day"
+touch "./$day/ex_input.txt"
+scriptPath="./$day/day$day.py"
 
 cat <<- EOF > $scriptPath
 from aocd import get_data
 from aocd import submit
 
-stringData = get_data(day=$1, year=$year)
+stringData = get_data(day=$day, year=$year)
 lines = stringData.splitlines()
 
 submitA=False
@@ -20,11 +31,11 @@ def main():
     print("The solution for part 1 is: {0}".format(part1Solution(lines)))
     if submitA == True:
         print("Submitting solution A to AoC:")
-        submit(part1Solution(lines), part="a", day=$1, year=$year)
+        submit(part1Solution(lines), part="a", day=$day, year=$year)
     print("The solution for part 2 is: {0}".format(part2Solution(lines)))
     if submitB == True:
         print("Submitting solution B to AoC:")
-        sumbit(part2Solution(lines), part="b", day=$1, year=$year)
+        sumbit(part2Solution(lines), part="b", day=$day, year=$year)
 
 def part1Solution(lines):
     
@@ -41,5 +52,5 @@ EOF
 echo "Activate python venv"
 source ./venv/bin/activate
 
-echo "Created directory: ./$1"
+echo "Created directory: ./$day"
 echo "Created script skeleton: $scriptPath"
